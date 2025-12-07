c96bfce4c570d6a623ebf2de047d2a705565d0a4
Mahendra Gautam Sharma <mahendragautam464@gmail.com>
2025-12-07 11:45:03 +0200

    added backend for user login and register

--- app.py
+++ app.py
@@ -2,6 +2,10 @@ from flask import Flask, Blueprint, render_template, request, redirect, make_res
 from flask_cors import CORS
 from dotenv import load_dotenv
 import uuid
+from db import create_user,get_database
+from werkzeug.security import generate_password_hash
+from werkzeug.security import check_password_hash
+from flask import session
 
 load_dotenv()
 
@@ -28,7 +32,10 @@ sessions = {}
 
 @app.route('/')
 def home():
-    return render_template("index.html")
+    user = None
+    if "user_id" in session:
+        user = {"first_name": session.get("user_first_name"),"last_name": session.get("user_last_name")}
+    return render_template("index.html", user=user)
 
 # Login Route
 
@@ -36,6 +43,70 @@ def home():
 def login():
     return render_template("login.html")
 
+@app.route('/user/login', methods=["GET","POST"])
+def user_login():
+    if request.method == "POST":
+        user_email = request.form.get("email")
+        user_password = request.form.get("password")
+        db = get_database()
+       
+        user = db.users.find_one({"email": user_email})
+        if not user:
+            flash("Email not found!", "error")
+            return redirect('/login')
+
+        if not check_password_hash(user["password"], user_password):
+            flash("Incorrect password!", "error")
+            return redirect('/login')
+
+        # Create session
+        session["user_id"] = str(user["_id"])
+        session["user_first_name"] = user["first_name"]
+        session["user_last_name"] = user["last_name"]
+
+        return redirect('/')
+
+    return render_template("login.html")
+
+# Logout Route
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    flash("Logged out successfully", "success")
+    return redirect('/login')
+
+
+# Register Route
+
+@app.route('/register', methods=["GET","POST"])
+def register():
+    if request.method == "POST":
+        first_name = request.form.get("first_name")
+        last_name = request.form.get("last_name")
+        user_email = request.form.get("email")
+
+        db = get_database()
+
+        # Check if email already exists
+        existing_user = db.users.find_one({"email": user_email})
+        if existing_user:
+            flash("Email already registered!", "error")
+            return redirect("/login")
+
+        user_password = request.form.get("password")
+        hashed_password = generate_password_hash(user_password)
+        new_user = {
+            "first_name": first_name,
+            "last_name": last_name,
+            "email": user_email,
+            "password": hashed_password,
+            "role":"user"
+        }
+        create_user(new_user)
+        flash("User register successfully!", "success")
+        return redirect('/login')
+        
 # Admin Routes
 
 @app.route('/admin')
