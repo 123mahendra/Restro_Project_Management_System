ccb7c8cc80d4609b51bd79f5eae6f5c7c96cdf55
Mahendra Gautam Sharma <mahendragautam464@gmail.com>
2025-12-12 12:12:49 +0200

    asd

--- utils/db.py
+++ utils/db.py
@@ -1,6 +1,8 @@
 from pymongo import MongoClient
 
-client = MongoClient("mongodb://localhost:27017/")
+# client = MongoClient("mongodb://localhost:27017/")
+
+client = MongoClient("mongodb+srv://gautammahendra464_db_user:eJHf5YAugjm8yNu4@mahendra464.s7mwp9j.mongodb.net/?appName=mahendra46")
 
 db = client["ecommerce"]
 
