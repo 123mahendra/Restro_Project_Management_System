96979d95f2a47daab7f26bb6e716246ee2d67690
Mahendra Gautam Sharma <mahendragautam464@gmail.com>
2025-12-12 12:10:14 +0200

    added mongo db live in db.py

--- db.py
+++ db.py
@@ -1,7 +1,8 @@
 from pymongo import MongoClient
 
 def get_database():
-    client = MongoClient("mongodb://localhost:27017/")
+    # client = MongoClient("mongodb://localhost:27017/")
+    client = MongoClient("mongodb+srv://gautammahendra464_db_user:eJHf5YAugjm8yNu4@mahendra464.s7mwp9j.mongodb.net/?appName=mahendra464")
     db = client["restro_project"]
     return db
 
